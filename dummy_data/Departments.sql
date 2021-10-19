b2bbcdf0fbb7e0e5108eaac23423b8c26bcc187f
noelmathewisaac <noelmathewisaac@gmail.com>
2021-10-20 02:04:56 +0800

    Add dummy data

---
+++ dummy_data/Departments.sql
@@ -0,0 +1,8 @@
+insert into Departments (did, dname) values (1, 'Sales');
+insert into Departments (did, dname) values (2, 'Engineering');
+insert into Departments (did, dname) values (3, 'Marketing');
+insert into Departments (did, dname) values (4, 'Business Development');
+insert into Departments (did, dname) values (5, 'Accounting');
+insert into Departments (did, dname) values (6, 'Research and Development');
+insert into Departments (did, dname) values (7, 'Legal');
+insert into Departments (did, dname) values (8, 'Human Resources');
---
+++ dummy_data/MeetingRooms.sql
@@ -0,0 +1,12 @@
+insert into MeetingRooms (room, floor, rname, did) values (1, 1, 'Fintone Meeting Room', 2);
+insert into MeetingRooms (room, floor, rname, did) values (1, 2, 'Daltfresh Meeting Room', 5);
+insert into MeetingRooms (room, floor, rname, did) values (1, 3, 'Namfix Meeting Room', 7);
+insert into MeetingRooms (room, floor, rname, did) values (1, 4, 'Zoom Meeting Room', 3);
+insert into MeetingRooms (room, floor, rname, did) values (2, 1, 'Span Meeting Room', 7);
+insert into MeetingRooms (room, floor, rname, did) values (2, 2, 'Mat Lam Tam Meeting Room', 8);
+insert into MeetingRooms (room, floor, rname, did) values (2, 3, 'Subin Meeting Room', 6);
+insert into MeetingRooms (room, floor, rname, did) values (2, 4, 'Bloom Meeting Room', 4);
+insert into MeetingRooms (room, floor, rname, did) values (3, 1, 'Sub-Ex Meeting Room', 5);
+insert into MeetingRooms (room, floor, rname, did) values (3, 2, 'Zoolab Meeting Room', 2);
+insert into MeetingRooms (room, floor, rname, did) values (3, 3, 'Tin Meeting Room', 1);
+insert into MeetingRooms (room, floor, rname, did) values (3, 4, 'Lotlux Meeting Room', 8);
