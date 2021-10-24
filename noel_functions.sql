fa6fc7cefea9667c6f53cf788adfb82e616adf57
noelmathewisaac <noelmathewisaac@gmail.com>
2021-10-24 21:01:23 +0800

    Add function descriptions and contraint numbers

--- noel_functions.sql
+++ noel_functions.sql
@@ -1,3 +1,4 @@
+-- This routine is used to add a new department. 
 -- Usage: SELECT * FROM add_department(9, 'Test Department');
 CREATE OR REPLACE FUNCTION add_department
     (IN did INT, IN dname VARCHAR(50))
@@ -8,6 +9,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- This routine is used to remove a department
 -- Usage: SELECT * FROM remove_department(9);
 CREATE OR REPLACE FUNCTION remove_department
     (IN _did INT)
@@ -19,6 +21,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+-- This routine is used to add a new room
 -- Usage: SELECT * FROM add_room (5, 4, 'Test', 2, 7);
 CREATE OR REPLACE FUNCTION add_room
     (IN _room INT, IN _floor INT, IN _rname VARCHAR(50), IN _did INT, IN _capacity INT)
@@ -30,8 +33,7 @@ BEGIN
     INSERT INTO MeetingRooms (room, floor, rname, did)
 	VALUES (_room, _floor, _rname, _did);
 	
-	-- Initialise a manager_eid with a manger in the same department and insert capacity into Updates
-    -- Note: Using a trigger instead is difficult as it is hard to pass the capacity parameter which can't be accessed from NEW 	
+	-- Initialise a random manager_eid with a manger in the same department and insert room capacity into Updates
 	SELECT eid FROM employees e WHERE e.eid IN (SELECT eid FROM Manager) AND e.did=_did LIMIT 1 INTO manager_eid;
 	INSERT INTO Updates (manager_eid, room, floor, date, new_cap)
 	VALUES (manager_eid, _room, _floor, CURRENT_DATE, _capacity);	
@@ -39,12 +41,17 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+-- This routine is used to search for available rooms.
+-- Returns: The routine returns a table containing all meeting rooms that are available from the start hour (inclusive) to the end hour (exclusive). 
+--          In other words, [start hour, end hour). Note that the number of hours may be greater than 1 hour and it must be available.
 -- Usage: SELECT * FROM search_room('10', '2021-07-20','11:00:00','12:00:00')
 CREATE OR REPLACE FUNCTION search_room
     (IN _capacity INT, IN _date DATE, IN _start_hour TIME, IN _end_hour TIME)
 RETURNS TABLE(floor INT, room INT, did INT, capacity INT) AS $$
 BEGIN 
     RETURN QUERY
+
+	-- Find the latest capacity update date for each room
 	WITH latest_update_date AS (
 		SELECT u.room, u.floor, max(date) FROM Updates u
 		GROUP BY u.room, u.floor
@@ -78,7 +85,10 @@ $$ LANGUAGE plpgsql;
 
 
 
--- Usage: SELECT * FROM view_future_meeting('2021-02-20',475)
+-- This routine is to be used by employee to find all future meetings this employee is going to have that are already approved.
+-- Returns: The routine returns a table containing all meetings that are already approved for which this employee is joining from the given start date onwards. 
+--          Note that the employee need not be the one booking this meeting room.
+-- Usage: SELECT * FROM view_future_meeting('2021-02-20', 475) 
 CREATE OR REPLACE FUNCTION view_future_meeting
     (IN _start_date DATE, IN _eid INT)
 
@@ -96,7 +106,12 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- This routine is used to join a booked meeting room. The employee ID is the ID of the employee that is joining the booked meeting room. 
+-- If the employee is allowed to join, the routine will process the join. Since an approved meeting
+-- cannot have a change in participants, the employee is not allowed to join an approved meeting.
 -- Usage SELECT * FROM join_meeting(4, 2, '2021-08-10','13:00:00', '14:00:00', 12);
+-- Contraints satified: [17], [19], [23], [26]
+-- Qn: Should a close contact be allowed to join meetings/book rooms?
 CREATE OR REPLACE FUNCTION join_meeting
     (IN _floor INT, _room INT, _date DATE, _start_hour TIME, _end_hour TIME, _eid INT)
 RETURNS VOID AS $$
@@ -114,10 +129,10 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION can_join_meeting()
 -- Constraints for Joining
--- 1. Can only join future meetings
--- 2. Can only join if no fever
--- 3. Cannot join approved meetings
--- 4. Employee should not have resigned
+-- 1. Can only join future meetings [26]
+-- 2. Can only join if no fever [19]
+-- 3. Cannot join approved meetings [23] 
+-- 4. Employee should not have resigned [34]
 -- 5. Meeting Room Capacity cannot be exceeded 
 RETURNS TRIGGER AS $$
 DECLARE
@@ -183,7 +198,10 @@ EXECUTE FUNCTION can_join_meeting();
 
 
 
+-- This routine is used to leave a booked meeting room. If this employee is not the meeting in the first place, then do nothing. 
+-- Otherwise, process the leave. Since an approved meeting cannot have a change in participants, the employee is not allowed to leave an approved meeting.
 -- Usage SELECT * FROM leave_meeting(4, 2, '2021-08-10','13:00:00', '14:00:00', 12);
+-- Contraints satified: [23]
 CREATE OR REPLACE FUNCTION leave_meeting
     (IN _floor INT, _room INT, _date DATE, _start_hour TIME, _end_hour TIME, _eid INT)
 RETURNS VOID AS $$
@@ -206,7 +224,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION can_leave_meeting()
 -- Constraints for Leaving
--- 1. Cannot leave approved meetings
+-- 1. Cannot leave approved meetings [23]
 RETURNS TRIGGER AS $$
 DECLARE
 	is_meeting_approved INT;
