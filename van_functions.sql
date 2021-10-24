b6102d05075ce6f509c29e4643dd9f49f7a4fe09
noelmathewisaac <noelmathewisaac@gmail.com>
2021-10-24 13:40:19 +0800

    Fix minor mistakes and add comments for van_functions

--- van_functions.sql
+++ van_functions.sql
@@ -6,100 +6,128 @@
 -- non_compliance(startDate, endDate)
 -- change_capacity(floor_in, room_in, date_in, m_eid)
 
--- Usage: declar_health(1, '2021--MM-DD', 37)
+-- This routine is used for daily declaration of temperature.
+-- Usage: SELECT * FROM declare_health(1, '2021-03-16', 37.6);
 DROP FUNCTION IF EXISTS declare_health;
-CREATE OR REPLACE FUNCTION declare_health(eid_in INT,
-                        date_in Date,
-                    temp_in NUMERIC)
+CREATE OR REPLACE FUNCTION declare_health(IN eid_in INT, IN date_in Date, IN temp_in NUMERIC)
 RETURNS VOID AS $$
-    BEGIN
-    INSERT INTO HealthDeclaration(date, eid, temp)
-    VALUES(date_in, eid_in, temp_in);
-    END;
-    $$ language plpgsql;
+BEGIN
+	INSERT INTO HealthDeclaration(date, eid, temp)
+	VALUES(date_in, eid_in, temp_in);
+END;
+$$ language plpgsql;
 
--- Usage: contact_tracing(318)
--- Returns: table of eid as cc_eid that are close contact with eid_in
+
+
+-- This routine is used for contact tracing.
+-- If employee has fever then:
+--   1. The employee is removed FROM all future meeting room booking, approved or not.
+--      - If the employee is the one booking the room, the booking is cancelled, approved or not.
+--      - This employee cannot book a room until they are no longer having fever 
+--   2. All employees in the same approved meeting room FROM the past 3 (i.e., FROM day D-3 to day D) days are contacted.
+--      - These employees (called close contacts) are removed FROM future meeting in the next 7 days (i.e., FROM day D to day D+7).
+-- Usage: SELECT * FROM contact_tracing(499, '2021-11-17')
+-- RETURNs: table of eid as cc_eid that are close contact with eid_in
 -- Does:
-    --remove eid_in from all future meetings
-    --cancel meeting eid_in booked
-    -- for all employees in same approved meeting room from past 3 days --> close contacts
-       -- removed from future meetings for next 7 days
-    /*
-*/
+    -- remove eid_in FROM all future meetings if they have fever
+    -- Cancel meeting eid_in booked
+    -- for all employees in same approved meeting room FROM past 3 days --> close contacts are removed FROM future meetings for next 7 days
 -- DOESNT WORK
-CREATE OR REPLACE FUNCTION contact_tracing(eid_in INT)
+CREATE OR REPLACE FUNCTION contact_tracing(IN eid_in INT, IN trace_date DATE)
 RETURNS TABLE(cc_eid INT) AS $$
 BEGIN
-    DELETE FROM Joins WHERE (eid = eid_in AND date >= current_date);
-    DELETE FROM Sessions WHERE (booker_eid = eid_in AND date >= current_date);
-    RETURN QUERY
-    WITH MeetingRoomsAffected as (
-        Select m.room, m.floor FROM MeetingRooms m NATURAL JOIN Joins j NATURAL JOIN Sessions s
-        WHERE j.eid = eid_in AND
-              j.date < current_date AND j.date >= current_date - INTERVAL '3 DAYS' AND
-              s.approver_eid is not NULL),
+    -- **check for fever before doing anything. If no fever do nothing
+	
+    -- Find all approved meetings FROM the past 3 days which employee was part of
+	WITH MeetingRoomsAffected as (
+        SELECT m.room, m.floor FROM MeetingRooms m NATURAL JOIN Joins j NATURAL JOIN Sessions s
+        WHERE j.eid = eid_in
+        AND j.date < trace_date AND j.date >= trace_date - INTERVAL '3 DAYS'
+        AND s.approver_eid IS NOT NULL
+	),
+	
+	-- Find close contacts: employees in the same approved meeting room FROM the past 3 (i.e., FROM day D-3 to day D) days
+	-- ** Should this have j.date < trace_date or j.date <= trace_date?? Change in the below part accordingly also!
     CloseContacts as (
-    SELECT j.eid FROM Joins j, MeetingRoomsAffected m
-        WHERE j.date <= current_date AND j.date >= current_date - INTERVAL '3 DAYS' AND
-              j.room = m.room AND j.floor = m.floor /*same room*/)
-    DELETE FROM Joins j where j.eid in (Select * from CloseContacts) AND
-                              j.date >= current_date AND j.date <= current_date + INTERVAL '7 DAYS';
-    with
-    MeetingRoomsAffected as (
-        Select m.room, m.floor FROM MeetingRooms m NATURAL JOIN Joins j NATURAL JOIN Sessions s
-        WHERE j.eid = eid_in AND
-              j.date < current_date AND j.date >= current_date - INTERVAL '3 DAYS' AND
-              s.approver_eid is not NULL)
+		SELECT DISTINCT j.eid FROM Joins j, MeetingRoomsAffected m
+		WHERE j.date < trace_date AND j.date >= trace_date - INTERVAL '3 DAYS'
+		AND j.room = m.room
+		AND j.floor = m.floor /*same room*/
+	)
+    
+	DELETE FROM Joins j WHERE j.eid IN (SELECT * FROM CloseContacts) 
+	AND j.date >= trace_date AND j.date <= trace_date + INTERVAL '7 DAYS';
+							  
+    RETURN QUERY
+	-- Find all approved meetings FROM the past 3 days which employee was part of
+	WITH MeetingRoomsAffected as (
+        SELECT m.room, m.floor FROM MeetingRooms m NATURAL JOIN Joins j NATURAL JOIN Sessions s
+        WHERE j.eid = eid_in
+        AND j.date < trace_date AND j.date >= trace_date - INTERVAL '3 DAYS'
+        AND s.approver_eid IS NOT NULL
+	),
+	
+	-- Find close contacts: employees in the same approved meeting room FROM the past 3 (i.e., FROM day D-3 to day D) days
+    CloseContacts as (
+		SELECT DISTINCT j.eid FROM Joins j, MeetingRoomsAffected m
+		WHERE j.date < trace_date AND j.date >= trace_date - INTERVAL '3 DAYS'
+		AND j.room = m.room
+		AND j.floor = m.floor /*same room*/
+	)
+	SELECT * FROM CloseContacts;
+	
+	-- The employee is removed FROM all future meeting room booking, approved or not.
+    DELETE FROM Joins WHERE (eid = eid_in AND date >= trace_date);
+    
+	--If the employee is the one booking the room, the booking is cancelled, approved or not.
+	DELETE FROM Sessions WHERE (booker_eid = eid_in AND date >= trace_date);
+	
+END;
+$$ LANGUAGE plpgsql;
 
-   SELECT j.eid FROM Joins j, MeetingRoomsAffected m
-        WHERE j.date <= current_date AND j.date >= current_date - INTERVAL '3 DAYS' AND
-              j.room = m.room AND j.floor = m.floor ;/*same room*/
---     LOOP
---         cc_eid := cc_record.cc_eid;
---
---     end loop;
-    END;
-    $$ language plpgsql;
-
-select * from contact_tracing(2);
 
 -- ??? CREATE TRIGGER TO RUN contact_tracing AFTER insert/update into healthDeclaration
 -- CREATE TRIGGER trigger_contact_tracing
 --     AFTER INSERT OR UPDATE ON healthdeclaration
 --     FOR EACH ROW EXECUTE FUNCTION contact_tracing();
---returns eid and num days not declared from [start, end] order by desc num_days
+
+-- This routine is used to find all employees that do not comply with the daily health declaration (i.e., to snitch).
+-- RETURNs: The routine RETURNs a table containing all employee ID that do not declare their temperature at least once FROM the start date 
+--          (inclusive) to the end date (inclusive). In other words, [start date, end date] along with 
+--           the number of days the employee did not declare their temperature within the given period. (ORDER BY DESC numDays )
+-- Usage: SELECT * FROM non_compliance('2021-11-17', '2021-11-18')
 DROP FUNCTION IF EXISTS non_compliance(startDate Date, endDate Date);
-CREATE OR REPLACE FUNCTION non_compliance(startDate Date, endDate Date) RETURNS TABLE(eid INT, days BIGINT) AS
-    $$
-    DECLARE
-        num_days INT;
-    BEGIN
-       num_days = endDate - startDate;
-        --count how many times each employee declared.
-        --return if count < numDays
-        RETURN QUERY
-       with num_declarations as
-           ( SELECT h.eid, count(*) as num_declared from HealthDeclaration h
-             WHERE h.date >= startDate and h.date <= endDate and h.temp is not null
-             group by h.eid
-             order by num_days asc) -- asc means eid with most temps declared first
-        select e.eid,
-               CASE WHEN e.eid NOT IN (select t.eid from num_declarations t)
-                   THEN num_days
-                   ELSE num_days - (select t.num_declared from num_declarations t where t.eid=e.eid)
-               END as num_undeclared
-            from Employees e order by num_undeclared desc ;
-    END;
-    $$ language plpgsql;
+CREATE OR REPLACE FUNCTION non_compliance(IN startDate Date, IN endDate Date) 
+RETURNS TABLE(eid INT, days BIGINT) AS $$
+DECLARE
+	num_days INT;
+BEGIN
+   --**added +1 for num_days
+   num_days = endDate - startDate + 1;
+   
+   RETURN QUERY
+   --count how many times each employee declared temperature and RETURN if count < numDays
+   -- **You have't removed cases WHERE count =num_days: can just remove cases WHERE days=0 in the final SELECT statement
+   WITH num_declarations AS ( 
+	     SELECT h.eid, count(*) AS num_declared FROM HealthDeclaration h
+		 WHERE h.date >= startDate AND h.date <= endDate AND h.temp IS NOT NULL
+		 GROUP BY h.eid
+	     ORDER by num_days asc --**Redundant since num_days in constant, if you meant num_declared then you are ordering in the next statement anyway so can remove this
+	   
+   ) -- asc means eid with most temps declared first
+   
+   SELECT e.eid,
+		  CASE WHEN e.eid NOT IN (SELECT t.eid FROM num_declarations t)
+			   THEN num_days
+			   ELSE num_days - (SELECT t.num_declared FROM num_declarations t WHERE t.eid=e.eid)
+		  END AS num_undeclared
+   FROM Employees e ORDER BY num_undeclared DESC;
+END;
+$$ language plpgsql;
 
--- dates: 2021-11-17  to 2021-11-19
-select * from non_compliance('2021-11-17', '2021-11-19');
 
-
-
-
-CREATE OR REPLACE FUNCTION change_capacity(floor_in int, room_in int, cap int, date_in Date, m_eid int)
+-- Usage: SELECT * FROM change_capacity(1, 1, 12, current_date, 499)
+CREATE OR REPLACE FUNCTION change_capacity(IN floor_in int, IN room_in int, IN cap int, IN date_in Date, IN m_eid int)
 RETURNS VOID AS
     $$
     BEGIN
@@ -108,44 +136,51 @@ RETURNS VOID AS
 $$ language plpgsql;
 
 -- Updating Constraints:
--- 1. Manager from same dept only can update capacity [24]
+-- 1. Manager FROM same dept only can update capacity [24]
 CREATE OR REPLACE FUNCTION do_updating_capacity() RETURNS TRIGGER AS $$
 BEGIN
- IF ( (select e.did FROM Employees e where e.eid = NEW.manager_eid) !=
-      (select m.did from MeetingRooms m NATURAL JOIN Updates u
-      WHERE m.room = NEW.room AND m.floor = NEW.floor))
-THEN
-    RAISE NOTICE 'Only managers from the same department can update capacity';
-    Return  NULL;
-ELSE
-     return NEW;
-END IF;
+    --** I got an error here because the right half of the equality can RETURN more than 1 row 
+	-- ("ERROR:  more than one row RETURNed by a subquery used as an expression")
+	-- This is because Updates has PK (room, floor, m_eid) so if a row is inserted by a different manager, 2 rows can have same room and floor
+	-- Need to rewrite this logic
+	IF ((SELECT e.did FROM Employees e WHERE e.eid = NEW.manager_eid) !=
+		  (SELECT m.did FROM MeetingRooms m NATURAL JOIN Updates u
+		  WHERE m.room = NEW.room AND m.floor = NEW.floor))
+	THEN
+		RAISE NOTICE 'Only managers FROM the same department can update capacity';
+		RETURN  NULL;
+	ELSE
+		 RETURN NEW;
+	END IF;
 END
-    $$ language plpgsql;
+$$ language plpgsql;
+
+
 
 DROP TRIGGER IF EXISTS trigger_before_new_cap ON Updates;
 CREATE TRIGGER trigger_before_new_cap
-    BEFORE INSERT OR UPDATE ON updates
-    FOR EACH ROW EXECUTE  FUNCTION do_updating_capacity();
+BEFORE INSERT OR UPDATE ON updates
+FOR EACH ROW 
+EXECUTE FUNCTION do_updating_capacity();
 
 
 -- Does: delete sessions which exceed the new capacity after it is updated.
 CREATE OR REPLACE FUNCTION post_updated_cap() RETURNS TRIGGER
 AS $$
-    BEGIN
-        --is there a trigger to delete from Joins if deleted in sessions? (Khiaxeng)
-        delete from Sessions s where (
-            s.floor = NEW.floor and s.room = NEW.room and s.date >= NEW.date and
-            (select count(j.eid) from Joins j
-            where j.floor = s.floor and j.room = s.room and j.time = s.time and j.date = s.date
-            group by (j.floor, j.room, j.time, j.date)) > NEW.new_cap) ;
-        return null;
-    END;
-    $$ language plpgsql;
+BEGIN
+    --is there a trigger to delete FROM Joins if deleted in sessions? (Khiaxeng)
+    delete FROM Sessions s WHERE (
+        s.floor = NEW.floor and s.room = NEW.room and s.date >= NEW.date and
+        (SELECT count(j.eid) FROM Joins j
+        WHERE j.floor = s.floor and j.room = s.room and j.time = s.time and j.date = s.date
+        GROUP BY (j.floor, j.room, j.time, j.date)) > NEW.new_cap) ;
+    RETURN NULL;
+END;
+$$ language plpgsql;
 
 DROP TRIGGER IF EXISTS trigger_after_new_cap ON Updates;
 CREATE TRIGGER trigger_after_new_cap
     AFTER INSERT OR UPDATE ON updates
     FOR EACH ROW EXECUTE FUNCTION post_updated_cap();
 
-select * from change_capacity(1,1,1,'2021-07-20', 475);
+SELECT * FROM change_capacity(1,1,1,'2021-07-20', 475);
