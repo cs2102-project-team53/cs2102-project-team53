be4cdc08f34f11425c8d39b461582530ab5bd709
vanshiqa <e0411071@u.nus.edu>
2021-10-26 12:57:02 +0800

    reviewed kevin's functions

--- kevin_functions.sql
+++ kevin_functions.sql
@@ -1,4 +1,7 @@
 -- Example usage: SELECT * FROM add_employee('kevin', 88735936, 'SENIOR', 1);
+-- ** Added a REPLACE(ename, ' ', '') so the spaces between first and last name are trimmed
+-- ** TO ADD: IF NAME ALREADY EXISTS, ADD NUMBER TO EMAIL ID.
+-- ** T0 ADD: DONT ADD TO EMPLOYEES IF KIND IS NOT 'JUNIOR', 'SENIOR' OR 'MANAGER'
 CREATE OR REPLACE FUNCTION add_employee
     (IN ename VARCHAR(50), IN mobile_number INT, IN kind VARCHAR(50), IN did INTEGER)
 RETURNS VOID AS $$
@@ -9,8 +12,9 @@ BEGIN
     SELECT COALESCE(MAX(eid), 0) INTO max_eid FROM Employees; -- Defaults to 0 if there are no rows yet
     new_eid := max_eid + 1;
 
-    INSERT INTO Employees (eid, ename, email, mobile_number, did)
-    VALUES (new_eid, ename, CONCAT(ename, '@gmail.com'), mobile_number, did);
+    -- ** ADD CHECK THAT KIND IS IETHER 'JUNIOR', 'SENIOR' 'MANAGER' FIRST
+    INSERT INTO Employees(eid, ename, email, mobile_number, did)
+    VALUES (new_eid, ename, CONCAT(REPLACE(ename, ' ', ''), '@gmail.com'), mobile_number, did);
 
     IF kind = 'JUNIOR' THEN
         INSERT INTO Junior (eid) VALUES (new_eid); -- idk if SQL smart enough to differentiate the two eid
@@ -27,46 +31,57 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- TESTING FUNCTIONS:
+-- SELECT * FROM add_employee('Vanshiqa', 90915145, 'JUNIOR', 1);
+-- SELECT * FROM add_employee('Anshiqa Agrawal', 90915145, 'JUNIOR', 1);
+-- SELECT * FROM add_employee('Charlie Agrawal', 90915145, 'JUNIOR', 3);
+-- SELECT * FROM add_employee('Charlie Agrawal', 90915145, 'JUNIOR', 3); -- check if unique email id generated w duplicate name
+-- SELECT * FROM add_employee('Charlie', 90915145, 'JUNIR', 3); -- mispelt kind.
+
 -- Usage: SELECT * FROM remove_employee(1, 'YYYY-MM-DD');
+-- ** What are you returning?
 CREATE OR REPLACE FUNCTION remove_employee
     (IN _eid INT, IN last_day DATE, OUT status INT)
 RETURNS INT AS $$
 BEGIN
 -- update resignedDate of employee with the given eid
     UPDATE Employees e
-    SET resignedDate = last_day
-    WHERE e.eid = _eid; -- idk if SQL smart enough to differentiate the two eid
+    SET resigned_date = last_day
+    WHERE e.eid = _eid;
 END;
 $$ LANGUAGE plpgsql;
 
+SELECT * FROM remove_employee(501, '2021-06-04');
 
 
 -- Usage: SELECT * FROM view_manager_report('2020-04-07', 1);
 CREATE OR REPLACE FUNCTION view_manager_report
     (IN start_date DATE, IN _eid INT)
-RETURNS TABLE(floor_number INT, room_number INT, date DATE, start_hour DOUBLE PRECISION, eid INT) AS $$
+RETURNS TABLE(floor_number INT, room_number INT, date DATE, start_hour DOUBLE PRECISION, booker_eid INT) AS $$
 DECLARE
     is_manager INT;
     department INT;
 BEGIN
     SELECT COUNT(*) INTO is_manager FROM Manager m WHERE m.eid = _eid;
+    -- ** WHY DU NEED TO ADD DEPARTMENT LOL CAN JUST DO SELECT E.DID WHERE E.EID = _EID?
     SELECT d.did INTO department FROM Employees e, Departments d WHERE e.eid=_eid AND e.did=d.did;
 
     IF is_manager = 0 THEN
         RETURN;
+    ELSE
+        RETURN QUERY
+        SELECT s.floor as floor_number, s.room as room_number, s.date, extract(hour from s.time) as start_hour, _eid as eid
+        FROM Sessions s NATURAL JOIN MeetingRooms m
+        WHERE s.approver_eid IS NULL
+        AND m.did=department
+        AND start_date <= s.date
+        ORDER BY s.date, s.time ASC;
     END IF;
-
-    RETURN query
-    SELECT s.floor as floor_number, s.room as room_number, s.date, extract(hour from s.time) as start_hour, _eid as eid
-    FROM Sessions s JOIN MeetingRooms m ON s.room=m.room
-    WHERE s.approver_eid IS NULL
-    AND m.did=department
-    AND start_date <= s.date
-    ORDER BY s.date, s.time ASC;
-
 END;
 $$ LANGUAGE plpgsql;
 
+-- TESTING QUERIES:
+-- SELECT * FROM view_manager_report('2021-02-04',500);
 
 -- If already in Junior, cannot be in Booker and vice versa
 CREATE OR REPLACE FUNCTION check_employees_exclusivity()
