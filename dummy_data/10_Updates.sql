880827a86f6c9beffe04217694e546e2c5d700f4
noelmathewisaac <noelmathewisaac@gmail.com>
2021-11-01 19:57:12 +0800

    Update dummy data

--- dummy_data/10_Updates.sql
+++ dummy_data/10_Updates.sql
@@ -1,16 +1,16 @@
 
 
 -- Updates
-insert into Updates (manager_eid, room, floor, date, new_cap) values (482, 1, 1, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (490, 1, 2, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (499, 1, 3, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (493, 1, 4, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (470, 2, 1, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (478, 2, 2, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (491, 2, 3, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (473, 2, 4, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (487, 3, 1, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (467, 3, 2, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (480, 3, 3, '2021-10-10', 10);
-insert into Updates (manager_eid, room, floor, date, new_cap) values (474, 3, 4, '2021-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (482, 1, 1, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (490, 1, 2, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (499, 1, 3, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (493, 1, 4, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (470, 2, 1, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (478, 2, 2, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (491, 2, 3, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (473, 2, 4, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (487, 3, 1, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (467, 3, 2, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (480, 3, 3, '2019-10-10', 10);
+insert into Updates (manager_eid, room, floor, date, new_cap) values (474, 3, 4, '2019-10-10', 10);
 
