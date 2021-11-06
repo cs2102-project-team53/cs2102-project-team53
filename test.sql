d2968d9c86f0a9ab3527f4e5eb330b46214f0450
vanshiqa <e0411071@u.nus.edu>
2021-11-06 22:28:28 +0800

    Added test.sqlfile

---
+++ test.sql
@@ -0,0 +1,10 @@
+-- FUNCTION: declare_health
+-- TEST CASE 1:
+-- SELECT * FROM declare_heath(500, '2021-10-20', 41.1)
+    -- Expectation: Will perform contact tracing
+
+
+-- MANUAL OPERATION: INSERT INTO HealthDeclaration
+-- TEST CASE 1:
+-- INSERT INTO HealthDeclaration (date, eid, temp) VALUES ('2021-12-31', 223, 41.2);
+    -- Expectation: ...
